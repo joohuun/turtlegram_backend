717e8796459824c0feac8c6fde6234f9be9b3eea
DESKTOP-57P8NLR\user <k00005781@gmail.com>
2022-05-18 16:16:46 +0900

    123

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+from bson import ObjectId
+from pymongo import MongoClient
 import hashlib
 import json
-from unittest import result
-from flask import Flask, jsonify, redirect, render_template, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from pymongo import MongoClient
+from datetime import datetime, timedelta
+import jwt
 
-client = MongoClient('localhost', 27017)
-db = client.turtle
+SECRET_KEY = 'turtle'
+
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+client = MongoClient('localhost', 27017)
+db = client.dbturtle
 
 
 @app.route("/")
@@ -16,50 +21,67 @@ def hello_world():
     return jsonify({'message': 'success'})
 
 
-@app.route("/signup", methods=["GET", "POST"])
+@app.route("/signup", methods=["POST"])
 def sign_up():
+    print(request)
+    print(request.form)
+    print(request.data)
     data = json.loads(request.data)
-    if request.method == "POST":
-        id = data.get("id")
-        pw = data.get("pw")
-        pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    print(data)
 
+    password = data['password']  # password = data.get('password', None)
+    password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+    exists = bool(db.user.find_one({"email": data['email']}))
+    if not exists:
         doc = {
-            "id": id,
-            "pw": pw_hash,
+            'email': data['email'],  # data.get('email')
+            'password': password_hash
         }
-
-        db.users.insert_one(doc)
-
-        return jsonify({"result": "회원가입 완료!!!"})
+        db.user.insert_one(doc)
+        return jsonify({'message': 'success'})
     else:
-        return redirect('index.html')
-        # return render_template('index.html')
+        print("중복되었습니다.")
+        return jsonify({'message': 'fail'})
 
 
-# @app.route("/signup", methods=["POST"])
-# def sign_up():
-    # # 1. form-data 입력 방식
-    # print(request)
-    # print(request.form)
-    # print(request.form['id'])  # id값을 못찾으면 오류남
-    # print(request.form.get('pw'))  # id값을 못찾아도 오류 안남
-    # return jsonify({'message': 'success'})
-
-    # # 2. json 입력 방식
-    # data = json.loads(request.data)
-    # print(data)
-    # print(data.get('id'))
-    # print(data.get('pw'))
-    # return jsonify({'message': 'success'})
-
-@app.route("/signup/check_id", methods=["POST"])
-def check_id():
+@app.route("/login", methods=["POST"])
+def sign_in():
+    print(request)
     data = json.loads(request.data)
-    id = data.get("id")
-    duplicated_id = db.users.find_one({'id': id})
+    print(data)
 
-    return jsonify({"duplicated": bool(duplicated_id)})
+    email = data.get("email")
+    password = data.get("password")
+    hashed_pw = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    print(hashed_pw)
+
+    result = db.user.find_one({"email": email, "password": hashed_pw})
+    print(result)
+
+    if result is None:
+        return jsonify({'message': '아이디나 비밀번호가 옳지 않습니다'}), 401
+
+    payload = {
+        "id": str(result["_id"]),
+        "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+    }
+
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    print(token)
+    return jsonify({'message': 'login', 'token': token})
+
+
+@app.route("/getuserinfo", methods=["GET"])
+def get_user_info():
+    # print("1.", request.headers) #header hidden
+    token = request.headers.get("Authorization")
+    print("2.", token)
+    user = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+    print("3.", user)
+    result = db.user.find_one({'_id': ObjectId(user["id"])})
+    print("4.", result)
+    return jsonify({"message": "success", "email": result['email']})
 
 
 if __name__ == '__main__':
